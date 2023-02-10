c9024b50e4e49a6320e3f7c6e58eb014f141fc36
liujun <jun.liu_southchina@convertlab.com>
2023-02-10 11:38:20 +0800

    factor add sso resource application

---
+++ oauth2-resource/src/main/java/com/orjrs/oauth2/resource/Oauth2ResourceApplication.java
@@ -0,0 +1,19 @@
+package com.orjrs.oauth2.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Oauth2 资源服务
+ *
+ * @author liujun
+ * @create 2023-02-10 11:13:12
+ */
+@SpringBootApplication
+public class Oauth2ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ResourceApplication.class, args);
+    }
+
+}
---
+++ oauth2-resource/src/main/java/com/orjrs/oauth2/resource/config/ResourceServerAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.orjrs.oauth2.resource.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+/**
+ * Oauth2 资源服务配置类
+ *
+ * @author liujun
+ * @create 2023-02-10 11:13:12
+ */
+@Configuration
+public class ResourceServerAutoConfiguration {
+
+    @Bean
+    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated().and()
+                .oauth2ResourceServer().jwt();
+
+        return http.build();
+    }
+}
