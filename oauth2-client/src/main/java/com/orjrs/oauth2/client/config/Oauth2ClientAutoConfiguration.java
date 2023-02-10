3fc9914219f6eb514273cc880cd337d668824aae
liujun <jun.liu_southchina@convertlab.com>
2023-02-10 10:59:36 +0800

    factor add sso client application

---
+++ oauth2-client/src/main/java/com/orjrs/oauth2/client/config/Oauth2ClientAutoConfiguration.java
@@ -0,0 +1,30 @@
+package com.orjrs.oauth2.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Oauth2 客户端配置类
+ *
+ * @author liujun
+ * @create 2023-02-10 10:40:23
+ */
+@Configuration
+public class Oauth2ClientAutoConfiguration {
+
+    @Bean
+    public SecurityFilterChain authorizationClientSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().logout()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and().oauth2Client()
+                .and().oauth2Login();
+
+        return http.build();
+    }
+}
---
+++ oauth2-client/src/main/java/com/orjrs/oauth2/client/controller/OauthClientDemoController.java
@@ -0,0 +1,26 @@
+package com.orjrs.oauth2.client.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Oauth2 客户端控制类
+ *
+ * @author liujun
+ * @create 2023-02-10 10:40:23
+ */
+@Slf4j
+@RestController
+public class OauthClientDemoController {
+
+    @RequestMapping(path = "/hello")
+    public String demo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("authentication: {}", authentication);
+
+        return "success !";
+    }
+}
